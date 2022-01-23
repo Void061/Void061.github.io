d49093d18486b5f4bfb7b9ea8879917640d9134d
bubble <bubblegame061@outlook.it>
2022-01-23 21:55:06 +0100

    New Commit

---
+++ _next/static/L8mrMMCKTuiJ5rPVE9SO9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-a17f9e6fc8d10b82.js","static/chunks/575-48d8c3d412687830.js","static/chunks/pages/index-e63f71c4ea18409e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
