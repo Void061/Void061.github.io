bb9a17252abb6c81aff19da45a3bb6790b0454f6
Ivan Liberato <57140583+Void061@users.noreply.github.com>
2022-07-14 21:47:01 +0200

    Works added

---
+++ _next/static/wV0r-DXKm4GfV3TFet-CO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-a17f9e6fc8d10b82.js","static/chunks/606-8e82c1ac18631c98.js","static/chunks/pages/index-0c8b477e071965d1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
